370b0e5f4ebe6db2a8894edfe84127a5902bd1e2
feliciasalim <feliciasalim08@gmail.com>
2024-03-18 12:43:02 +0700

    POS

---
+++ src/utspbo/Barang.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package utspbo;
+
+/**
+ *
+ * @author ASUS
+ */
+public class Barang {
+    public String kode;
+    public float harga;
+    public String nama;
+    
+}
--- src/utspbo/UTSPBO.java
+++ src/utspbo/UTSPBO.java
@@ -14,7 +14,8 @@ public class UTSPBO {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        POSFrame frame = new POSFrame();
+        frame.setVisible(true);
     }
     
 }
